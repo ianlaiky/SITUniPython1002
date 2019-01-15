336f30d14f8c56bc7ee2e8de7558340a70e2ffd7
Ian <152772a@mymail.nyp.edu.sg>
2019-01-15 16:58:33 +0800

    i

---
+++ Lab1/Task3/calculator.py
@@ -0,0 +1,42 @@
+import sys
+
+try:
+    a = str(sys.argv[1])
+    b = float(sys.argv[2])
+    c = float(sys.argv[3])
+except:
+    print "Your input is invalid!"
+if (float(b) >0.0 and float(c)>0.0):
+    if (str(a).lower() == "metric".lower()) or (str(a).lower() == "imperial".lower()):
+        height = float(b)
+        weight = float(c)
+        bmi = 0.0
+        if str(a).lower() == "metric".lower():
+            bmi = weight / height ** 2
+        if str(a).lower() == "imperial".lower():
+            bmi = 703 * weight / height ** 2
+
+        bmi = "%.2f" % bmi
+        cat = ""
+        if bmi < float(16):
+            cat = "Severe Thinness"
+        elif float(bmi) >=float(17) and float(bmi) <=float(17):
+            cat = "Moderate Thinness"
+        elif float(bmi) >=float(17) and float(bmi) <= float(18.5):
+            cat = "Mild Thinness"
+        elif float(bmi) >=float(18.5) and float(bmi) <=float(25):
+            cat = "Normal"
+        elif float(bmi) >=float(25) and float(bmi) <= float(30):
+            cat = "Overweight"
+        elif float(bmi) >=float(30) and float(bmi) <=float(35):
+            cat = "Obese Class I"
+        elif float(bmi) >=float(35) and float(bmi) <=float(40):
+            cat = "Obese Class II"
+        elif float(bmi) > float(40):
+            cat = "Obese Class III"
+
+        print str(bmi) + " " + cat
+    else:
+        print "Your input is invalid!"
+else:
+    print "Your input is invalid!"
