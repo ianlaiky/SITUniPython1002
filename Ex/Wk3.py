143f44802a7a3b7817d1e8e6a6ec9afd1607e0cc
Ian <152772a@mymail.nyp.edu.sg>
2019-01-22 09:53:56 +0800

    i

---
+++ Ex/Wk3.py
@@ -0,0 +1,21 @@
+# sal = [1000,2500,2600,500,14]
+#
+# print sal
+#
+# sal.append(30)
+#
+# print sal.pop(sal.index(min(sal)))
+# removedSal = sal.pop(sal.index(min(sal)))
+
+
+
+#ex 2
+
+list1 = [5,7,8,9]
+
+list2 = list(list1)
+
+list2[0] =10
+
+print list1
+print list2
