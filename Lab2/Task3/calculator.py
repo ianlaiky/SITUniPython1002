4cf9883400d60219a01a7e76bf548c2401a0b841
Ian <152772a@mymail.nyp.edu.sg>
2019-01-22 18:36:41 +0800

    i

---
+++ Lab2/Task3/calculator.py
@@ -0,0 +1,22 @@
+import sys
+from collections import Counter
+
+a=""
+try:
+    a = str(sys.argv[1])
+
+except:
+
+    print "Your input is invalid!"
+
+word = Counter(str(a).lower())
+count = 0
+wordlist = ""
+# print word
+
+for oiw in sorted(word, key=word.get, reverse=True):
+    count = count +1
+    wordlist = str(wordlist)+","+oiw+":"+str(word[oiw])
+    if count ==5:
+        break
+print wordlist[1:]
