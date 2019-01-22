04adab30c7dee25a99a5b5e5d622455f219dd4b1
Ian <152772a@mymail.nyp.edu.sg>
2019-01-22 18:11:12 +0800

    i

--- Lab2/Task2/calculator.py
+++ Lab2/Task2/calculator.py
@@ -1,6 +1,7 @@
 import sys
 
 a = ""
+error= False
 try:
     a = str(sys.argv[1])
 
@@ -10,31 +11,38 @@ except:
 
 listt = str(a).split(",")
 list = []
-for i in listt:
-    list.append(int(i))
+try:
+    for i in listt:
+
+        list.append(int(i))
+except:
+    error = True
+    print "Please enter valid integers."
+
 
 # print list
+if not error:
 
-sumOfEvenNumbers = 0
-sumOfOddNumbers = 0
-listEven = []
-listOdd = []
-for i in list:
-    if int(i) % 2 == 0:
-        listEven.append(int(i))
-        sumOfEvenNumbers = sumOfEvenNumbers + int(i)
-    else:
-        listOdd.append(int(i))
-        sumOfOddNumbers = sumOfOddNumbers + int(i)
+    sumOfEvenNumbers = 0
+    sumOfOddNumbers = 0
+    listEven = []
+    listOdd = []
+    for i in list:
+        if int(i) % 2 == 0:
+            listEven.append(int(i))
+            sumOfEvenNumbers = sumOfEvenNumbers + int(i)
+        else:
+            listOdd.append(int(i))
+            sumOfOddNumbers = sumOfOddNumbers + int(i)
 
-differenceBetweenBigSmall = int(max(list)) - int(min(list))
+    differenceBetweenBigSmall = int(max(list)) - int(min(list))
 
-sumall = int(sumOfEvenNumbers) + int(sumOfOddNumbers)
-# print int(min(list))
-minussumall = int(sumall)-(int(max(list))+int(min(list)))
-# print minussumall
-avg = minussumall/(len(list)-2)
+    sumall = int(sumOfEvenNumbers) + int(sumOfOddNumbers)
+    # print int(min(list))
+    minussumall = int(sumall)-(int(max(list))+int(min(list)))
+    # print minussumall
+    avg = minussumall/(len(list)-2)
 
-print "The sum of all even numbers is " + str(sumOfEvenNumbers) + ", the sum of all odd numbers is " + str(
-    sumOfOddNumbers) + ", the difference between the biggest and smallest number is " + str(differenceBetweenBigSmall) + ",the total number of even numbers is " + str(
-    str(len(listEven))) + ", the total number of odd numbers is " + str(len(listOdd))+", the centered average is "+str(avg)
+    print "The sum of all even numbers is " + str(sumOfEvenNumbers) + ", the sum of all odd numbers is " + str(
+        sumOfOddNumbers) + ", the difference between the biggest and smallest number is " + str(differenceBetweenBigSmall) + ",the total number of even numbers is " + str(
+        str(len(listEven))) + ", the total number of odd numbers is " + str(len(listOdd))+", the centered average is "+str(avg)
