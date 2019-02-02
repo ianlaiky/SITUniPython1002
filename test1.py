864023e4359faa6099062b67f87a70b4b4890271
Ian <152772a@mymail.nyp.edu.sg>
2019-02-02 23:23:39 +0800

    i

--- test1.py
+++ test1.py
@@ -3,3 +3,10 @@ def say(message, times = 2):
 
 say('Hello')
 say('World', 5)
+
+def func(a, b=5, c=10):
+    print('a is', a, 'and b is', b, 'and c is', c)
+
+func(3, 7)
+func(25, c = 24)
+func(c = 50, a = 100)
