6b3d5ffeec787cca6e83d1b91818298304407236
Ian <152772a@mymail.nyp.edu.sg>
2019-02-02 23:30:57 +0800

    i

--- test1.py
+++ test1.py
@@ -10,3 +10,19 @@ def func(a, b=5, c=10):
 func(3, 7)
 func(25, c = 24)
 func(c = 50, a = 100)
+
+def func(a, b, names):
+    a = a+10
+    b = b+20
+    names[0] = 12
+    names[1] = 18
+    return a,b
+
+x,y=10,30
+
+fruits= ['apple', 'orange', 'banana']
+
+num1, num2=func(x,y,fruits)
+print num1, num2
+for fruit in fruits:
+    print fruit
