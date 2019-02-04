dbc93587b9c1fcde25178fa2c6e8e8a9b754ab52
Ian <152772a@mymail.nyp.edu.sg>
2019-02-04 23:55:00 +0800

    i

--- Lab4/Task3/elfish.py
+++ Lab4/Task3/elfish.py
@@ -1,17 +1,22 @@
-def elfish(x):
+import sys
 
+a = str(sys.argv[1])
+
+
+def elfish(x):
     if str(x) == "":
         return x
 
     else:
         # print x
         if x[0] == "e":
-            return elfish(x[1:])+x[0]
+            return elfish(x[1:]) + x[0]
         if x[0] == "l":
-            return elfish(x[1:])+x[0]
+            return elfish(x[1:]) + x[0]
         if x[0] == "f":
-            return elfish(x[1:])+x[0]
+            return elfish(x[1:]) + x[0]
         else:
             return elfish(x[1:])
 
-print elfish("walesff")
+
+print str(a) + " is " + 'one elfish word!' if str((''.join(sorted(elfish(''.join(set(a))))))) == "efl" else str(a)+ ' is not an' + " elfish word!"
